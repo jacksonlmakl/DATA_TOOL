71dbbea6c23faebeb6f57ca23864126a262e13d5
Jackson Makl <jackson.makl@dataiku.com>
2024-06-26 04:45:29 +0000

    f

--- sync_dags.sh
+++ sync_dags.sh
@@ -15,7 +15,7 @@ find $DAGS_DIR -type f -name "dag.py" | while read -r DAG_FILE; do
     ln -s "$DAG_FILE" "$LINK_NAME"
 
     echo "Symbolic links created for all DAG files."
-    cp -R ~/DATA_TOOL/dags/${DAG_NAME}/configuration.yaml ~/DATA_TOOL/dags/${DAG_NAME}_configuration.yaml
+    cp -R ~/DATA_TOOL/dags/${DAG_NAME}/${DAG_NAME}_configuration.yaml ~/DATA_TOOL/dags/${DAG_NAME}_configuration.yaml
     sudo apt-get update
     sudo apt-get install -y build-essential cmake libre2-dev
     source ~/DATA_TOOL/airflow_venv/bin/activate && pip install -r ~/DATA_TOOL/dags/${DAG_NAME}/requirements.txt
