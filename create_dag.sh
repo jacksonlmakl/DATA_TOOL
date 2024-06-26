1cecfe94882950ed56024c4ca8661599417d3626
Jackson Makl <jackson.makl@dataiku.com>
2024-06-26 04:43:57 +0000

    f

--- create_dag.sh
+++ create_dag.sh
@@ -9,4 +9,5 @@ cp -R ~/DATA_TOOL/dag_templates/dag.py ~/DATA_TOOL/dags/${DAG_NAME}/dag.py
 sed -i "s/*<PLACEHOLDER_NAME_HERE>&/${DAG_NAME}/g" ~/DATA_TOOL/dags/${DAG_NAME}/dag.py
 cp -R ~/DATA_TOOL/dag_templates/requirements.txt ~/DATA_TOOL/dags/${DAG_NAME}/requirements.txt
 cp -R ~/DATA_TOOL/dag_templates/configuration.yaml ~/DATA_TOOL/dags/${DAG_NAME}/configuration.yaml
+sed -i "s/*<PLACEHOLDER_NAME_HERE>&/${DAG_NAME}/g" ~/DATA_TOOL/dags/${DAG_NAME}/configuration.yaml
 ./sync_dags.sh
